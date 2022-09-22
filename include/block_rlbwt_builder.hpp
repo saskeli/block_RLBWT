32d8ced69e549762e5d811c242c4a6ab25893c50
Saska Dönges <tuomionpupu@gmail.com>
2022-09-22 11:42:09 +0300

    breaking refactor started

--- include/block_rlbwt_builder.hpp
+++ include/block_rlbwt_builder.hpp
@@ -4,53 +4,80 @@
 #include <vector>
 
 namespace bbwt {
-template <class block_type, uint32_t cap, class bwt_type>
+template <class partial_block_type, class alphabet_type, uint32_t cap, class bwt_type>
 class block_rlbwt_builder {
   private:
-    std::vector<uint8_t> heads_;
-    std::vector<uint8_t> runs_;
-    std::vector<block_type> blocks_;
+    typedef partial_block_type<alphabet_type<uint32_t>> block_type;
+    static const constexpr uint64_t BLOCKS_IN_SUPER_BLOCK = (uint64_t(1) << 32) / cap;
+
+    std::string prefix_;
+    std::string suffix_;
+    std::vector<alphabet_type<uint64_t>> block_counts_;
+    alphabet_type<uint64_t> super_block_cumulative_;
+    uint64_t super_block_count_;
+    uint64_t super_block_bytes_;
+    uint64_t super_block_size_;
+    uint64_t super_block_elems_;
+    uint8_t* current_super_block_;
+    uint8_t** scratch_;
+    block_type current_block_;
     uint32_t block_elems_;
+    uint32_t block_bytes_;
+
   public:
-    block_rlbwt_builder() : heads_(), runs_(), blocks_(), block_elems_(0) {}
+    block_rlbwt_builder(std::string out_file) {
+        size_t loc = out_file.find_last_of('.');
+        if (loc == std::string::npos) {
+            prefix_ = out_file;
+            suffix_ = "";
+        } else {
+            prefix_ = out_file.substr(0, loc);
+            suffix_ = out_file.substr(loc);
+        }
+        block_counts_ = std::vector<alphabet_type<uint64_t>>();
+        super_block_count_ = alphabet_type<uint64_t>();
+        super_block_count_ = 0;
+        super_block_bytes_ = sizeof(alphabet_type);
+        super_block_size_ = BLOCKS_IN_SUPER_BLOCK * (block_type::min_size() + sizeof(alphabet_type));
+        super_block_elems_ = 0;
+        current_super_block_ = (uint64_t*)calloc(super_block_size_, 1);
+        scratch_ = (uint8_t**)malloc(block_type::scratch_blocks() * sizeof(uint8_t*));
+        for (size_t i = 0; i < block_type::scratch_blocks(); i++) {
+            scratch_[i] = (uint8_t*)malloc(block_type::scratch_size(i));
+        }
+        current_block_ = block_type();
+        block_elems_ = 0;
+        block_bytes_ = sizeof(block_type);
+    }
 
     void append(uint8_t head, uint32_t length) {
-        heads_.push_back(head);
-        if (block_elems_ + length > cap) [[unlikely]] {
-            uint32_t overflow = block_elems_ + length - cap;
-            write(cap - block_elems_);
+        if (length + block_elems_ < cap) [[likely]] {
+            alphabet()->add(head, length);
+            block_bytes_ += current_block_->append(head, length, scratch_);
+        } else if (length + block_elems_ == cap) [[unlikely]] {
+            alphabet()->add(head, length);
+            block_bytes_ += current_block_->append(head, length, scratch_);
             commit();
-            heads_.push_back(head);
-            write(overflow);
-            block_elems_ = overflow;
         } else {
-            write(length);
-            block_elems_ += length;
-        }
-        if (block_elems_ == cap) [[unlikely]] {
+            uint32_t fill = cap - block_elems_;
+            alphabet()->add(head, fill);
+            block_bytes_ += current_block_->append(head, fill, scratch_);
             commit();
+            return append(head, length - fill);
         }
     }
 
-    bwt_type compile() {
+    void finalize() {
         commit();
-        return bwt_type(blocks_);
     }
   private:
-    void write(uint32_t length) {
-        while (length) {
-            uint8_t b = length & 0b01111111;
-            length >>= 7;
-            uint8_t c = length ? 0b0 : 0b10000000;
-            runs_.push_back(b | c);
-        }
-    }
 
     void commit() {
-        blocks_.push_back({heads_, runs_});
-        heads_.clear();
-        runs_.clear();
-        block_elems_ = 0;
+        if ()
+    }
+
+    alphabet_type* alphabet() {
+        return reinterpret_cast<alphabet_type*>(current_super_block_ + (super_block_bytes_ - sizeof(alphabet_type)));
     }
 };    
 } // namespace bbwt
